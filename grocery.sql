ef588e7b2f9cee71b621add4141a7be9014cffd0
javedakhtar2003 <122269037+javedakhtar2003@users.noreply.github.com>
2024-05-15 07:59:46 +0530

    grocery.sql

--- grocery.sql
+++ grocery.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.1
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 10, 2024 at 12:05 PM
--- Server version: 10.4.32-MariaDB
--- PHP Version: 8.0.30
+-- Generation Time: May 15, 2024 at 04:28 AM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.2.0
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -69,6 +69,71 @@ INSERT INTO `items` (`ItemID`, `Name`, `Price`, `Quantity`, `Expiry Date`, `Mfg 
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `logistic`
+--
+
+CREATE TABLE `logistic` (
+  `shipment_id` int(10) NOT NULL,
+  `order_id` int(10) NOT NULL,
+  `user_id` int(10) NOT NULL,
+  `delivery_type` varchar(10) NOT NULL,
+  `delivery_charges` decimal(5,0) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `order_details`
+--
+
+CREATE TABLE `order_details` (
+  `order_id` int(10) NOT NULL,
+  `user_id` int(10) NOT NULL,
+  `order_date_time` datetime NOT NULL,
+  `total_amount` decimal(15,0) NOT NULL,
+  `item_qty` int(12) NOT NULL,
+  `item_price` decimal(21,0) NOT NULL,
+  `item_id` int(10) NOT NULL,
+  `item_name` varchar(30) NOT NULL,
+  `order_status` varchar(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `payment`
+--
+
+CREATE TABLE `payment` (
+  `payment_id` int(11) NOT NULL,
+  `user_id` int(10) NOT NULL,
+  `order_id` int(10) NOT NULL,
+  `payment_method` varchar(20) NOT NULL,
+  `payment_status` varchar(20) NOT NULL,
+  `payment_amount` decimal(7,0) NOT NULL,
+  `payment_date_time` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `report`
+--
+
+CREATE TABLE `report` (
+  `report_id` int(10) NOT NULL,
+  `from_date` date NOT NULL,
+  `to_date` date NOT NULL,
+  `item_id` int(10) NOT NULL,
+  `item_name` varchar(30) NOT NULL,
+  `profit_loss` decimal(7,0) NOT NULL,
+  `item_qty` int(10) NOT NULL,
+  `total_sold_amount` decimal(7,0) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `user`
 --
 
@@ -111,6 +176,37 @@ ALTER TABLE `items`
   ADD PRIMARY KEY (`ItemID`);
 
 --
+-- Indexes for table `logistic`
+--
+ALTER TABLE `logistic`
+  ADD PRIMARY KEY (`shipment_id`),
+  ADD KEY `fk_order_id` (`order_id`),
+  ADD KEY `fk_users_id` (`user_id`);
+
+--
+-- Indexes for table `order_details`
+--
+ALTER TABLE `order_details`
+  ADD PRIMARY KEY (`order_id`),
+  ADD KEY `fk_us_id` (`user_id`),
+  ADD KEY `fk_it_id` (`item_id`);
+
+--
+-- Indexes for table `payment`
+--
+ALTER TABLE `payment`
+  ADD PRIMARY KEY (`payment_id`),
+  ADD KEY `fk_usr_id` (`user_id`),
+  ADD KEY `fk_ord_id` (`order_id`);
+
+--
+-- Indexes for table `report`
+--
+ALTER TABLE `report`
+  ADD PRIMARY KEY (`report_id`),
+  ADD KEY `fk_its_id` (`item_id`);
+
+--
 -- Indexes for table `user`
 --
 ALTER TABLE `user`
@@ -133,10 +229,44 @@ ALTER TABLE `items`
   MODIFY `ItemID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
+-- AUTO_INCREMENT for table `logistic`
+--
+ALTER TABLE `logistic`
+  MODIFY `shipment_id` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `order_details`
+--
+ALTER TABLE `order_details`
+  MODIFY `order_id` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `payment`
+--
+ALTER TABLE `payment`
+  MODIFY `payment_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `report`
+--
+ALTER TABLE `report`
+  MODIFY `report_id` int(10) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
   MODIFY `ID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `payment`
+--
+ALTER TABLE `payment`
+  ADD CONSTRAINT `fk_ord_id` FOREIGN KEY (`order_id`) REFERENCES `order_details` (`order_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
