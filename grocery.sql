6e8acfddc79fa5ea0759ecf1bd817066ee4d725b
javedakhtar2003 <122269037+javedakhtar2003@users.noreply.github.com>
2024-05-17 13:27:28 +0530

    grocery.sql added

--- grocery.sql
+++ grocery.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.1
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 14, 2024 at 06:31 AM
--- Server version: 10.4.32-MariaDB
--- PHP Version: 8.0.30
+-- Generation Time: May 17, 2024 at 09:56 AM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.2.0
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -34,6 +34,15 @@ CREATE TABLE `cart` (
   `quantity` int(5) NOT NULL,
   `user_mobile` varchar(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `cart`
+--
+
+INSERT INTO `cart` (`ID`, `item_name`, `price`, `quantity`, `user_mobile`) VALUES
+(1, 'potato', 25, 1, '1234567890'),
+(3, 'Lemon', 20, 1, ' 123456789'),
+(9, 'Potato', 25, 1, ' 123456789');
 
 -- --------------------------------------------------------
 
@@ -86,6 +95,53 @@ INSERT INTO `items` (`ItemID`, `Name`, `Price`, `Quantity`, `Expiry_Date`, `Mfg_
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `logistic`
+--
+
+CREATE TABLE `logistic` (
+  `shipment_id` int(10) NOT NULL,
+  `order_id` int(10) NOT NULL,
+  `mobile` varchar(10) NOT NULL,
+  `delivery_type` varchar(10) NOT NULL,
+  `delivery_charges` decimal(5,0) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `payment`
+--
+
+CREATE TABLE `payment` (
+  `payment_id` int(11) NOT NULL,
+  `mobile` varchar(10) NOT NULL,
+  `order_id` int(10) NOT NULL,
+  `payment_method` varchar(20) NOT NULL,
+  `payment_status` varchar(20) NOT NULL,
+  `payment_amount` decimal(7,0) NOT NULL,
+  `payment_date_time` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `report`
+--
+
+CREATE TABLE `report` (
+  `report_id` int(10) NOT NULL,
+  `from_date` date NOT NULL,
+  `to_date` date NOT NULL,
+  `item_id` int(10) NOT NULL,
+  `item_name` varchar(30) NOT NULL,
+  `profit_loss` decimal(7,0) NOT NULL,
+  `item_qty` int(10) NOT NULL,
+  `total_sold_amount` decimal(7,0) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `user`
 --
 
@@ -102,7 +158,8 @@ CREATE TABLE `user` (
 
 INSERT INTO `user` (`fname`, `lname`, `pwd`, `mobile`) VALUES
 ('Aditya', 'Srivastava', '12345', '1234567890'),
-('Admin', 'Admin', '123456', '7235047914');
+('Admin', 'Admin', '123456', '7235047914'),
+('Javed', 'Akhtar', '122333', '9967444540');
 
 --
 -- Indexes for dumped tables
@@ -127,6 +184,29 @@ ALTER TABLE `items`
   ADD PRIMARY KEY (`ItemID`);
 
 --
+-- Indexes for table `logistic`
+--
+ALTER TABLE `logistic`
+  ADD PRIMARY KEY (`shipment_id`),
+  ADD KEY `fk_order_id` (`order_id`),
+  ADD KEY `fk_users_id` (`mobile`);
+
+--
+-- Indexes for table `payment`
+--
+ALTER TABLE `payment`
+  ADD PRIMARY KEY (`payment_id`),
+  ADD KEY `fk_usr_id` (`mobile`),
+  ADD KEY `fk_ord_id` (`order_id`);
+
+--
+-- Indexes for table `report`
+--
+ALTER TABLE `report`
+  ADD PRIMARY KEY (`report_id`),
+  ADD KEY `fk_its_id` (`item_id`);
+
+--
 -- Indexes for table `user`
 --
 ALTER TABLE `user`
@@ -140,7 +220,7 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `cart`
 --
 ALTER TABLE `cart`
-  MODIFY `ID` int(10) NOT NULL AUTO_INCREMENT;
+  MODIFY `ID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT for table `category`
@@ -153,6 +233,24 @@ ALTER TABLE `category`
 --
 ALTER TABLE `items`
   MODIFY `ItemID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `logistic`
+--
+ALTER TABLE `logistic`
+  MODIFY `shipment_id` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `payment`
+--
+ALTER TABLE `payment`
+  MODIFY `payment_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `report`
+--
+ALTER TABLE `report`
+  MODIFY `report_id` int(10) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
